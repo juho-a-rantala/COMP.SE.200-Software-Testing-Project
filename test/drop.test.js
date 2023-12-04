52802724e5a57a646acaeb4cdaa296fe4b5439b5
bkhera <henri.r.rantala@tuni.fi>
2023-12-04 16:14:30 +0200

    drop, should expect an array

--- test/drop.test.js
+++ test/drop.test.js
@@ -16,17 +16,17 @@ describe('drop.js - slice an array from the given distance from beginning', func
     it('Should splice off number of elements given by distance', function (){
         expect(drop(mixedArray, 3)).to.eql([ 'apple','orange', true]);
     });
-    it('Should return empty with integer with default splice', function (){
-        expect(drop(1)).to.eql([]);
+    it('Should return an array with integer with default splice', function (){
+        expect(drop(1)).to.be.a('array');
     });
-    it('Should return empty with integer with distance 0', function (){
-        expect(drop(1, 0)).to.eql([]);
+    it('Should return an array with integer with distance 0', function (){
+        expect(drop(1, 0)).to.be.a('array');
     });
-    it('Should return empty with string', function (){
-        expect(drop('string')).to.eql([]);
+    it('Should return an array with string', function (){
+        expect(drop('string')).to.be.a('array');
     });
-    it('Should return empty with null', function (){
-        expect(drop(null)).to.eql([]);
+    it('Should return an array with null', function (){
+        expect(drop(null)).to.be.a('array');
     });
     it('Should return original when distance < 0', function (){
         expect(drop(mixedArray, -2)).to.eql(mixedArray);
